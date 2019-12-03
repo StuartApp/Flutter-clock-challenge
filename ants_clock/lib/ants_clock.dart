b9acbaa8e4a7ed9be0002d2e698f7ae635d6b87c
Pau Picas <p.picas@stuart.com>
2019-12-03 11:58:50 +0100

    Initial commit

---
+++ ants_clock/lib/ants_clock.dart
@@ -0,0 +1,82 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/model.dart';
+
+class AntsClock extends StatefulWidget {
+  const AntsClock(this.model);
+
+  final ClockModel model;
+
+  @override
+  _AntsClockState createState() => _AntsClockState();
+}
+
+class _AntsClockState extends State<AntsClock> {
+  DateTime _dateTime = DateTime.now();
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.addListener(_updateModel);
+    _updateTime();
+    _updateModel();
+  }
+
+  @override
+  void didUpdateWidget(AntsClock oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.model != oldWidget.model) {
+      oldWidget.model.removeListener(_updateModel);
+      widget.model.addListener(_updateModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    widget.model.removeListener(_updateModel);
+    widget.model.dispose();
+    super.dispose();
+  }
+
+  void _updateModel() {
+    setState(() {
+      // Cause the clock to rebuild when the model changes.
+    });
+  }
+
+  void _updateTime() {
+    setState(() {
+      _dateTime = DateTime.now();
+      // Update once per minute. If you want to update every second, use the
+      // following code.
+      _timer = Timer(
+        Duration(minutes: 1) -
+            Duration(seconds: _dateTime.second) -
+            Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
+      // Update once per second, but make sure to do it at the beginning of each
+      // new second, so that the clock is accurate.
+      // _timer = Timer(
+      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+      //   _updateTime,
+      // );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      alignment: Alignment.center,
+      child: Text(_dateTime.toIso8601String()),
+    );
+  }
+}
---
+++ ants_clock/lib/main.dart
@@ -0,0 +1,25 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
+
+import 'ants_clock.dart';
+
+void main() {
+  // A temporary measure until Platform supports web and TargetPlatform supports
+  // macOS.
+  if (!kIsWeb && Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS.
+    // https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
+  runApp(ClockCustomizer((ClockModel model) => AntsClock(model)));
+}
